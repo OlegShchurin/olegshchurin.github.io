e6e8b9d1381aa9af3082f0b274dd99cf5da5c6a0
Oleg <Olegshchurin@gmail.com>
2020-10-16 10:10:52 +0300

    carousel block

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.carousel__slider').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/prev_arrow.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/next_arrow.svg"></button>'
+    });
+
+
+
+
+  });
