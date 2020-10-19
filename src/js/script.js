ec4c81930535928ec6bfdeda2fdceff6a77daff5
Oleg <Olegshchurin@gmail.com>
2020-10-19 09:59:18 +0300

    add catalog_tabs

--- src/js/script.js
+++ src/js/script.js
@@ -3,8 +3,4 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/prev_arrow.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/next_arrow.svg"></button>'
     });
-
-
-
-
   });
